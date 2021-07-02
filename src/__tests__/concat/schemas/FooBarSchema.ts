6fc6653586a99dfeb3aa69c345d832ea43128319
mrjono1 <jonoclarnette@gmail.com>
2021-07-02 20:18:48 +1000

    fix issue with joi.concat() add test for when using enums as an example of how to do it

---
+++ src/__tests__/concat/schemas/FooBarSchema.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+export const FooSchema = Joi.object({
+  a: Joi.string()
+}).meta({ className: 'Foo' });
+
+export const BarSchema = Joi.object({
+  b: Joi.string()
+}).meta({ className: 'Bar' });
+
+export const FooBarSchema = FooSchema
+  .concat(BarSchema)
+  .meta({ className: 'FooBar' });
