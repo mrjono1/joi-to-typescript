deeff982c2a2c349e5645451959bbf79785f3345
David Tymon <david.tymon@bendigoadelaide.com.au>
2021-07-07 09:06:03 +1000

    Add additional test to show debug when index file is ignored

--- src/__tests__/ignoreIndexFiles/ignoreIndexFiles.ts
+++ src/__tests__/ignoreIndexFiles/ignoreIndexFiles.ts
@@ -50,4 +50,27 @@ describe('Ignore Index Files', () => {
     expect(/^export \* from '.\/Address';$/gm.test(indexContents)).toBeTruthy();
     expect(/^export \* from '.\/Person';$/gm.test(indexContents)).toBeTruthy();
   });
+
+  test('Ignores index files if requested with debug', async () => {
+    const ignoreIndexFiles = true;
+    const debug = true;
+    const result = await convertFromDirectory({
+      schemaDirectory,
+      typeOutputDirectory,
+      ignoreIndexFiles,
+      debug
+    });
+
+    expect(result).toBe(true);
+
+    expect(existsSync(`${typeOutputDirectory}/Person.ts`)).toBeTruthy();
+    expect(existsSync(`${typeOutputDirectory}/Address.ts`)).toBeTruthy();
+
+    // Index should exist but should just be exporting the definitions in the
+    // other generated files.
+    expect(existsSync(`${typeOutputDirectory}/index.ts`)).toBeTruthy();
+    const indexContents = readFileSync(`${typeOutputDirectory}/index.ts`, 'utf8');
+    expect(/^export \* from '.\/Address';$/gm.test(indexContents)).toBeTruthy();
+    expect(/^export \* from '.\/Person';$/gm.test(indexContents)).toBeTruthy();
+  });
 });
